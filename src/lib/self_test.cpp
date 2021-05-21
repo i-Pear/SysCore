9999c58d392229321ff037b965bbf8858f115c81
ipear <i.pear@outlook.com>
2021-05-21 10:44:08 +0800

    feat: add tester

---
+++ src/lib/self_test.cpp
@@ -0,0 +1,8 @@
+#include "self_test.h"
+
+int test_cnt=0;
+const int test_total=1;
+char* tests[test_total]={
+        "write"
+};
+
---
+++ src/lib/self_test.h
@@ -0,0 +1,8 @@
+#ifndef OS_RISC_V_SELF_TEST_H
+#define OS_RISC_V_SELF_TEST_H
+
+extern int test_cnt;
+extern int test_total;
+extern char* tests[];
+
+#endif //OS_RISC_V_SELF_TEST_H
