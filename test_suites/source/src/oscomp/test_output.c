b43edce054528db0f8c13c65e925ce1b7e5690fa
ipear <434317543@qq.com>
2021-05-28 14:54:13 +0800

    add: a simple calculator for output test

--- test_suites/source/src/oscomp/test_output.c
+++ test_suites/source/src/oscomp/test_output.c
@@ -1,11 +1,19 @@
 #include "stdio.h"
 
-int main(int argc, char *argv[]){
+int input_int(){
     char c;
+    int res=0;
     while (1){
         c=getchar();
-        if(c!='\n')putchar(c);
-        else break;
+        if(c>='0'&&c<='9')res=res*10+c-'0';
+        else return res;
     }
+}
+
+int main(int argc, char *argv[]){
+    printf("input two int, enter to end:\n");
+    int a=input_int();
+    int b=input_int();
+    printf("sum is %d\n",a+b);
     return 0;
 }
