c4688b8e7481386108cbe226f1901cfd6c5fc491
ipear <434317543@qq.com>
2021-07-01 21:30:43 +0800

    test: add page pooling
    Former-commit-id: 7532f06092b3fc9d8878b689ecfcff80754f52ba

--- src/kernel/elf_loader.h
+++ src/kernel/elf_loader.h
@@ -4,9 +4,21 @@
 #include <elf.h>
 #include "stdio.h"
 #include "memory/kernel_heap.h"
+#include "../lib/stl/list.h"
 
 //#define DEBUG_ELF
 
 void load_elf(const char* elf_data,int size,size_t* elf_page_base,size_t* elf_page_size,size_t* entry);
+
+struct ELF_Segment{
+    size_t file_offset;
+    size_t memory_offset;
+    size_t file_size;
+    size_t memory_size;
+};
+
+struct ELF_Info{
+    List<ELF_Segment> segments;
+};
 
 #endif //OS_RISC_V_ELF_LOADER_H
--- src/kernel/memory/process_memory.h
+++ src/kernel/memory/process_memory.h
@@ -5,16 +5,24 @@
 #include "../lib/stl/map.h"
 #include "memory.h"
 #include "page_pool.h"
+#include "../elf_loader.h"
 
 class Process_memory_controller{
+
     String elf_path;
+
+    // page table base
     size_t* page_table;
 
+    // stack
+    size_t stack_base;
     size_t stack_size;
-    size_t elf_page_size;
-    size_t stack;
+
+    // elf segment data
+    ELF_Info elf_segments;
 
     // virtual addr -> real addr
+    // include: heap(can edit) / elf(.data can edit) / file(using mmap)
     Map<size_t,Mmap_unit> map;
 
 };
