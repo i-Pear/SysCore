44e011032d2502ffa6296ba211e20f63cfbd0054
Waitti <waitti@foxmail.com>
2021-08-01 16:55:09 +0800

    feat: delete unused code
    Former-commit-id: 664b90f82a54c3dce8f594d011e6359f08c04255

--- src/kernel/interrupt.cpp
+++ src/kernel/interrupt.cpp
@@ -36,77 +36,77 @@ Context *handle_interrupt(Context *context, size_t scause, size_t stval) {
         }
         case 2:{
             if(is_interrupt == 0){
-                mtl("illegal ins");
-                lty(context->sepc);
-                lty(context->stval);
-                lty(context->satp);
-
-                size_t satp = context->satp;
-
-                size_t vir_addr = context->sepc;
-                size_t table_base = satp << 12;
-                lty(vir_addr);
-                lty(table_base);
-
-                size_t ppn1 = (vir_addr & (0b111111111LL << 30)) >> 30;
-                size_t ppn2 = (vir_addr & (0b111111111LL << 21)) >> 21;
-                size_t ppn3 = (vir_addr & (0b111111111LL << 12)) >> 12;
-                size_t offset = (vir_addr & (0xfff));
-                lty(ppn1);
-                lty(ppn2);
-                lty(ppn3);
-                lty(offset);
-
-                size_t* pte1 = (size_t *)table_base + ppn1;
-                lty(*pte1);
-
-                if(*pte1 == 0){
-                    size_t new_addr = alloc_page(4096);
-                    memset((char *) new_addr, 0, 4096);
-                    table_base = new_addr;
-                    lty(new_addr);
-                    *pte1 = ((new_addr >> 12) << 10) | 0xd1;
-                    lty(*pte1);
-                }else{
-                    lty(*pte1);
-                    table_base = (*pte1 >> 10) << 12;
-                    lty(table_base);
-                }
-
-                size_t* pte2 = (size_t*)table_base + ppn2;
-                lty(*pte2);
-
-                if(*pte2 == 0){
-                    size_t new_addr = alloc_page(4096);
-                    memset((char *) new_addr, 0, 4096);
-                    table_base = new_addr;
-                    lty(new_addr);
-                    *pte2 = ((new_addr >> 12) << 10) | 0xd1;
-                    lty(*pte2);
-                }else{
-                    lty(*pte2);
-                    table_base = (*pte2 >> 10) << 12;
-                    lty(table_base);
-                }
-
-                size_t* pte3 = (size_t*)table_base + ppn3;
-                lty(*pte3);
-
-                if(*pte3 == 0){
-//        size_t new_addr = elf_exec_page_base_only_one + ((vir_addr >> 12) << 12);
-                    size_t new_addr = alloc_page(4096);
-                    memset((char *) new_addr, 0, 4096);
-                    lty(new_addr);
-                    *pte3 = ((new_addr >> 12) << 10) | 0xdf;
-                    lty(*pte3);
-                }else{
-                    lty(*pte3);
-                    table_base = (*pte3 >> 10) << 12;
-                    lty(table_base);
-                }
-
-                lty(table_base + offset);
-                lty(*((size_t *)table_base + offset));
+//                mtl("illegal ins");
+//                lty(context->sepc);
+//                lty(context->stval);
+//                lty(context->satp);
+//
+//                size_t satp = context->satp;
+//
+//                size_t vir_addr = context->sepc;
+//                size_t table_base = satp << 12;
+//                lty(vir_addr);
+//                lty(table_base);
+//
+//                size_t ppn1 = (vir_addr & (0b111111111LL << 30)) >> 30;
+//                size_t ppn2 = (vir_addr & (0b111111111LL << 21)) >> 21;
+//                size_t ppn3 = (vir_addr & (0b111111111LL << 12)) >> 12;
+//                size_t offset = (vir_addr & (0xfff));
+//                lty(ppn1);
+//                lty(ppn2);
+//                lty(ppn3);
+//                lty(offset);
+//
+//                size_t* pte1 = (size_t *)table_base + ppn1;
+//                lty(*pte1);
+//
+//                if(*pte1 == 0){
+//                    size_t new_addr = alloc_page(4096);
+//                    memset((char *) new_addr, 0, 4096);
+//                    table_base = new_addr;
+//                    lty(new_addr);
+//                    *pte1 = ((new_addr >> 12) << 10) | 0xd1;
+//                    lty(*pte1);
+//                }else{
+//                    lty(*pte1);
+//                    table_base = (*pte1 >> 10) << 12;
+//                    lty(table_base);
+//                }
+//
+//                size_t* pte2 = (size_t*)table_base + ppn2;
+//                lty(*pte2);
+//
+//                if(*pte2 == 0){
+//                    size_t new_addr = alloc_page(4096);
+//                    memset((char *) new_addr, 0, 4096);
+//                    table_base = new_addr;
+//                    lty(new_addr);
+//                    *pte2 = ((new_addr >> 12) << 10) | 0xd1;
+//                    lty(*pte2);
+//                }else{
+//                    lty(*pte2);
+//                    table_base = (*pte2 >> 10) << 12;
+//                    lty(table_base);
+//                }
+//
+//                size_t* pte3 = (size_t*)table_base + ppn3;
+//                lty(*pte3);
+//
+//                if(*pte3 == 0){
+////        size_t new_addr = elf_exec_page_base_only_one + ((vir_addr >> 12) << 12);
+//                    size_t new_addr = alloc_page(4096);
+//                    memset((char *) new_addr, 0, 4096);
+//                    lty(new_addr);
+//                    *pte3 = ((new_addr >> 12) << 10) | 0xdf;
+//                    lty(*pte3);
+//                }else{
+//                    lty(*pte3);
+//                    table_base = (*pte3 >> 10) << 12;
+//                    lty(table_base);
+//                }
+//
+//                lty(table_base + offset);
+//                lty(*((size_t *)table_base + offset));
 
                 shutdown();
             }
