dcb66f9991db058de44bb8e84c8d36a35f12da1b
ipear <434317543@qq.com>
2021-08-01 20:53:08 +0800

    fix: change uname to linux
    Former-commit-id: 58f58857ab463acc9cbdb6f9e0ec9518d9ee79ec

--- src/kernel/posix/posix_structs.cpp
+++ src/kernel/posix/posix_structs.cpp
@@ -1,8 +1,8 @@
 #include "posix_structs.h"
 
 struct ES_utsname ES_uname={
-        "sysCore",
-        "node",
+        "Linux",
+        "debian",
         "5.10.0",
         "5.10.0",
         "riscv-64",
